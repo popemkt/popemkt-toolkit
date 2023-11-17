9963f27566974252ca71f79e570257bc91af8646
Nguyen Gia Hoang <popemkt1@gmail.com>
2023-11-17 16:26:04 +0700

    Update uninstaller

--- ProcRespawn/uninstall.sh
+++ ProcRespawn/uninstall.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-rm -rf "/home/popemkt/.hoang-toolkit/proc-respawn"
-rm /usr/local/bin/ProcRespawn
-
-systemctl disable proc-respawn.service
-systemctl stop proc-respawn.service
-rm "/etc/systemd/system/proc-respawn.service"
-systemctl reset-failed
-systemctl daemon-reload
+CURRENT_USER=$(whoami)
+HOME_PATH=$(echo $HOME)
+rm -rf "$HOME_PATH/.hoang-toolkit/proc-respawn"
+rm "$HOME_PATH/.config/autostart/proc-respawn.desktop"
+#systemctl disable proc-respawn.service
+#systemctl stop proc-respawn.service
+#rm "/etc/systemd/system/proc-respawn.service"
+#systemctl reset-failed
+#systemctl daemon-reload
 
 
